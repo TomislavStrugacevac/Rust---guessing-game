69ed94056dbf6ff0f5a76ce779eabcf119d926b3
Tomislav Strugačevac <tomislav.strugacevac@gauss.hr>
2020-07-27 22:44:12 +0200

    feat: minor error handling, added smiley face :)

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,45 @@ fn main() {
     println! ("-------------------------");
 
 
-    let mut guess = String::new();  // create mutable string guess to store user's input
+    
     let secret_number = rand::thread_rng().gen_range(1,101); // generate random secret_number
     
     println! ("Secret number is {}", secret_number);
+    let mut guess = String::new();  // create mutable string guess to store user's input
+    
+    loop {                                  // loop until user guesses the number
 
+    guess.clear();                          // clear the variable or on loop or it will append the new entries and cause panic      
     println!("Please enter the number");
 
     io::stdin()
         .read_line(&mut guess)              // store the input into reference of mutable guess
         .expect("Failed to read the line"); // if fails print out this message
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");  //convert user's string input into number
+    let guess: u32 = match guess.trim().parse() {           // convert user's string input into number
+        Ok(num) => num,                                     // on number return the number
+        Err(_) => {                                         // on other entries ignore the entry
+            println! ("This is not a number. Try again."); 
+            continue; 
+        }                                                
+    };
+       
+        println! ("You guessed {}", guess); // print user's input
 
-    println! ("You guessed {}", guess); // print user's input
+        match guess.cmp(&secret_number) {   // compare user input with secret_number reference
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {            // if guess is correct break out of the loop
+                println!("You win!");
+                println!("  ******** ");
+                println!(" *  O  O  *");
+                println!("*          *");
+                println!("*  \\____/  *");
+                println!(" *        *");
+                println!("  ********  ");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+                break;
+            }
+        }
     }
 }
