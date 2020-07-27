3cafd4322cca7c37e1abff59f657e9a87a8abd50
Tomislav Strugačevac <tomislav.strugacevac@gauss.hr>
2020-07-27 21:34:17 +0200

    feat: first user input

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::io;
 
 fn main() {
 
-    println!("Hello, world!");
     println!("Please enter the number");
+
+    let mut guess = String::new();  // create mutable string guess to store user's input
+
+    io::stdin()
+        .read_line(&mut guess)              // store the input into reference of mutable guess
+        .expect("Failed to read the line"); // if fails print out this message
+
+    print! ("You guessed {}", guess); // print user's input
 }
